bb2e2e995945c425dcb0866c093c00b6ec2bef1b
victorMwatu <victormwatu@gmail.com>
2025-06-10 11:01:22 +0300

    complete array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    let win = record.find( (arr) => arr.result === 'W');
+    if( win === undefined) {
+        return win;
+    }else {
+        return win.year;
+
+    }
+}
